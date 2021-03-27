609f351cfa6b59c578d1d149fcd8fe45d261d813
Michael Eliot <mieliot@protonmail.com>
2021-03-26 21:29:36 -0700

    feat: have link click through on only text

--- src/components/main/view/PollView.tsx
+++ src/components/main/view/PollView.tsx
@@ -42,42 +42,42 @@ export default function PollView({ poll }: { poll: any }) {
   const sum = options.reduce(reducer, 0);
 
   return (
-    <Link href={`/polls/${poll.id}`}>
-      <div
-        key={`poll-${poll.id}`}
-        className="p-5 card m-2 cursor-pointer border border-gray-400 rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200"
-      >
+    <div
+      key={`poll-${poll.id}`}
+      className="p-5 card m-2 cursor-pointer border border-gray-400 rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200"
+    >
+      <Link href={`/polls/${poll.id}`}>
         {`${poll.title}: ${poll.description}`}
-        <form>
-          {options.map((option: any) => (
-            <div
-              key={`option-${option.id}`}
-              className="relative bg-gray-100 m-1 w-100 h-100 rounded-lg"
-            >
-              <div className="flex relative z-10">
-                <input
-                  name="optionId"
-                  id={option.id}
-                  key={option.id}
-                  type="radio"
-                  value={option.id}
-                  defaultChecked={option.selected}
-                  onClick={() => onSubmit(option.id)}
-                />
-                <label
-                  htmlFor={option.id}
-                >{`${option.description} - Votes: ${option.votes}`}</label>
-              </div>
-              <div
-                style={{
-                  width: option.votes ? `${(option.votes / sum) * 100}%` : 0,
-                }}
-                className="absolute bg-gray-300 inset-0 z-0 rounded-lg"
+      </Link>
+      <form>
+        {options.map((option: any) => (
+          <div
+            key={`option-${option.id}`}
+            className="relative bg-gray-100 m-1 w-100 h-100 rounded-lg z-30"
+          >
+            <div className="flex relative z-10">
+              <input
+                name="optionId"
+                id={option.id}
+                key={option.id}
+                type="radio"
+                value={option.id}
+                defaultChecked={option.selected}
+                onClick={() => onSubmit(option.id)}
               />
+              <label
+                htmlFor={option.id}
+              >{`${option.description} - Votes: ${option.votes}`}</label>
             </div>
-          ))}
-        </form>
-      </div>
-    </Link>
+            <div
+              style={{
+                width: option.votes ? `${(option.votes / sum) * 100}%` : 0,
+              }}
+              className="absolute bg-gray-300 inset-0 z-0 rounded-lg"
+            />
+          </div>
+        ))}
+      </form>
+    </div>
   );
 }
